99086c4422e6cc9cec11f48c096a03577110fc16
Keshav Pratap Singh Rathore <144621639+KesBhu21@users.noreply.github.com>
2025-10-23 00:01:15 +0530

    Update init.js

--- init.js
+++ init.js
@@ -13,35 +13,22 @@ async function main() {
 
 let allChats = [
   {
-    from: "Keshav",
-    to: "Vini",
-    msg: "Hey Vini! How’s your day going?",
+    from: "Alice",
+    to: "Bob",
+    msg: "Hey Bob! How’s your day going?",
     created_at: new Date()
-  },
-  {
-    from: "Vini",
-    to: "Keshav",
+},
+{
+    from: "Bob",
+    to: "Alice",
     msg: "Heyy! It’s been good 😄 Just a bit busy with work.",
     created_at: new Date()
-  },
-  {
-    from: "Keshav",
-    to: "Vini",
-    msg: "Haha same here! Missing our evening walks though 😅",
-    created_at: new Date()
-  },
-  {
-    from: "Vini",
-    to: "Keshav",
-    msg: "Aww, we’ll go this weekend for sure 🌸",
-    created_at: new Date()
-  },
-  {
-    from: "Keshav",
-    to: "Vini",
-    msg: "Deal 😎 I’ll bring coffee this time!",
-    created_at: new Date()
-  }
+},
+{
+    from: "Alice",
+    to: "Bob",
+    msg: "Haha same here! Missing our evening walks though 😅"
+
 ];
 
 
